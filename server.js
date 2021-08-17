4c543db29400f87d509bb906bb8b2a67617269e9
abderrezak bkz <abderrezak.bkz@gmail.com>
2021-08-17 23:25:32 +0200

    intial commit to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = require('serve-static')
 const path = require('path')
 
 const app = express()
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
+app.use('/',server(path.join(__dirname,'/dist')))
 
 const port = process.env.PORT || 8080
 
